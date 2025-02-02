81ce138ceafb0fff147e915c240695fcc047c9b2
SnSnLi <128010647+SnSnLi@users.noreply.github.com>
2025-02-03 00:07:51 +0800

    Add files via upload

---
+++ load.py
@@ -0,0 +1,9 @@
+from modelscope import snapshot_download, AutoTokenizer
+from transformers import AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForSeq2Seq
+import torch
+# 在modelscope上下载GLM模型到本地目录下
+model_dir = snapshot_download("ZhipuAI/glm-4-9b-chat", cache_dir="./", revision="master")
+
+# Transformers加载模型权重
+tokenizer = AutoTokenizer.from_pretrained("./ZhipuAI/glm-4-9b-chat/", use_fast=False, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("./ZhipuAI/glm-4-9b-chat/", device_map="auto", torch_dtype=torch.bfloat16, trust_remote_code=True)
