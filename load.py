9e5bc43b688cefe168fa1b1dcc4aaa55d1933caa
SnSnLi <128010647+SnSnLi@users.noreply.github.com>
2025-02-17 10:35:04 +0800

    Delete load.py

--- load.py
+++
@@ -1,9 +0,0 @@
-from modelscope import snapshot_download, AutoTokenizer
-from transformers import AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForSeq2Seq
-import torch
-# 在modelscope上下载GLM模型到本地目录下
-model_dir = snapshot_download("ZhipuAI/glm-4-9b-chat", cache_dir="./", revision="master")
-
-
-tokenizer = AutoTokenizer.from_pretrained("./ZhipuAI/glm-4-9b-chat/", use_fast=False, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("./ZhipuAI/glm-4-9b-chat/", device_map="auto", torch_dtype=torch.bfloat16, trust_remote_code=True)
